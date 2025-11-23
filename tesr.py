9ad1a27ce8e96891bcaeb2385d7281664cbe4eb5
CaptianBob1 <164006402+CaptianCoder@users.noreply.github.com>
2025-11-23 14:05:13 -0500

    Update tesr.py

--- tesr.py
+++ tesr.py
@@ -5,3 +5,19 @@ circle = Circle(Point(250, 250), 100)
 circle.setFill("blue")
 circle.draw(win)
 
+while True:
+    while True:
+        move = circle.getCenter().getX() + 10
+        if move != 0:
+            circle.move(10, 0)
+            False
+            break
+
+    while True:
+        move = circle.getCenter().getX() - 10
+        if move != 500:
+            circle.move(-10, 0)
+            False
+            break
+
+    
